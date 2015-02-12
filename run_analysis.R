dcc1aecfaff17b89c2d7507f3a9669686f3da3d0
crhoffmeister <chris.hoffmeister@gmail.com>
2015-02-11 21:16:35 -0500

    Initial Commit
    Obtaining data from source.

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+# Run Analysis on the Human Activity Recognition Using 
+# Smartphones Dataset Version 1.0
+# ==================================================================
+# Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto.
+# Smartlab - Non Linear Complex Systems Laboratory
+# DITEN - Universit? degli Studi di Genova.
+# Via Opera Pia 11A, I-16145, Genoa, Italy.
+# activityrecognition@smartlab.ws
+# www.smartlab.ws
+# ==================================================================
+
+# Obtain data from source:
+
+if (!file.exists("./data")) {dir.create("./data")}
+source_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+getdata <- function() {
+      download.file(source_url, destfile = "./data/data.zip", method="curl")
+      dateDownloaded <- date()
+      unzip("./data/data.zip", exdir="./data")
+}
+
+# Merges the training and the test sets to create one data set.
+
+
+# Extracts only the measurements on the mean and standard deviation 
+# for each measurement. 
+
+
+# Uses descriptive activity names to name the activities in the data set.
+
+
+# Appropriately labels the data set with descriptive variable names. 
+
+
+# From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
+
