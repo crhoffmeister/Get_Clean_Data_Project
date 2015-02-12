145bc36d314f633a93bf3ce9e954c96a567efcb8
crhoffmeister <chris.hoffmeister@gmail.com>
2015-02-11 22:14:13 -0500

    update: merged data sets
    two data sets rbind combined.

--- run_analysis.R
+++ run_analysis.R
@@ -15,13 +15,15 @@ if (!file.exists("./data")) {dir.create("./data")}
 source_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
 getdata <- function() {
-      download.file(source_url, destfile = "./data/data.zip", method="curl")
+      download.file(source_url, destfile = "./data/data.zip", method="wb")
       dateDownloaded <- date()
       unzip("./data/data.zip", exdir="./data")
 }
 
 # Merges the training and the test sets to create one data set.
-
+train <- read.table("./Data/UCI HAR Dataset/train/X_train.txt")
+test <- read.table("./Data/UCI HAR Dataset/test/X_test.txt")
+merged <- rbind(train, test)
 
 # Extracts only the measurements on the mean and standard deviation 
 # for each measurement. 
@@ -29,10 +31,13 @@ getdata <- function() {
 
 # Uses descriptive activity names to name the activities in the data set.
 
+      # y_test.txt & activity_labels.txt, bind subject_test.txt
 
 # Appropriately labels the data set with descriptive variable names. 
 
+      # features.txt
 
 # From the data set in step 4, creates a second, independent tidy data set 
 # with the average of each variable for each activity and each subject.
 
+      # melt by subject & activity
